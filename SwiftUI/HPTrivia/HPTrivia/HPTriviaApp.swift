470e11238957c8155d73f1917e691e4e2a9f8ec1
haroldstest <dongik369@naver.com>
2025-06-23 06:10:30 +0900

    project init

---
+++ SwiftUI/HPTrivia/HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Dongik Song on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
